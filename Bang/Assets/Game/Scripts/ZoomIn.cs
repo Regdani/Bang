fb2bea16dcbac829f8e34f07660fd506e36f67fd
Regényi Dániel <regdani0330@gmail.com>
2018-11-29 12:50:20 +0100

    zoom&cards

---
+++ Bang/Assets/Game/Scripts/ZoomIn.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomIn : MonoBehaviour {
+
+    public static string zoomActive = "n";
+    void Start () {
+		
+	}
+	
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            zoomActive = "y";
+        }
+    }
+}
---
+++ Bang/Assets/Game/Scripts/ZoomOut.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomOut : MonoBehaviour {
+
+	
+	void Start () {
+        GetComponent<Camera>().enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ZoomIn.zoomActive == "y")
+        {
+            GetComponent<Camera>().enabled = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            GetComponent<Camera>().enabled = false;
+            ZoomIn.zoomActive = "n";
+        }
+    }
+}
